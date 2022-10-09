55b6ad4111415e28dd19d563cb48225ea8fa4c87
Ekaterina <ekaterina.dony@yandex.ru>
2022-10-09 13:34:47 +0300

    изменен вью конфиг

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-<<<<<<< HEAD
     ? '/gitogram2/dist/'
-=======
-    ? '/gitogram/dist/'
->>>>>>> master
     : '/'
 })
