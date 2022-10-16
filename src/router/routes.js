cf7c356157e1d87c81816faf03bb3cd2f2defb9f
Ekaterina Dony <ekaterina.dony@yandex.ru>
2022-10-16 19:00:17 +0300

    изменены настройки роутера

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,9 +2,7 @@ import NotFound from '@/components/NotFound.vue'
 import Feeds from '@/pages/feeds/Feeds.vue'
 const Stories = () => import('@/pages/Stories.vue')
 export default [{
-  path: process.env.NODE_ENV === 'production'
-    ? '/gitogram2/dist/'
-    : '/',
+  path: '/',
   name: 'home',
   component: Feeds
 },
